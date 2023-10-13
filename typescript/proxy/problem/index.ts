bf5f1506506564a56ef438782f8ebd6e9a0b776f
Franco de Desarrollo <francoegi01@gmail.com>
2023-10-13 14:27:04 -0300

    refactor(ts): sleep time proxy pattern

--- typescript/proxy/problem/index.ts
+++ typescript/proxy/problem/index.ts
@@ -3,12 +3,12 @@ import USERS from '../users.json'
 export class ApiUsers implements IApiUsers {
   private readonly users = USERS
   public async getUsers() {
-    await sleep(200)
+    await sleep(50)
     return this.users
   }
 
   public async getUser(id: number) {
-    await sleep(200)
+    await sleep(50)
     return this.users.find((user) => user.id === id)
   }
 }
