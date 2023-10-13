632c5e37df5f7150797fd06ec6a87dd056d0e1da
Franco de Desarrollo <francoegi01@gmail.com>
2023-10-13 14:22:50 -0300

    test(ts): observer solution and problem

---
+++ typescript/observer/problem/observer-problem.test.ts
@@ -0,0 +1,30 @@
+import { Customer, ShoesStore } from '.'
+
+describe('Observer problem', () => {
+  it('should be buy a shoe', () => {
+    const store = new ShoesStore()
+    const c1 = new Customer('John', store)
+    const c2 = new Customer('Jane', store)
+    const c3 = new Customer('Jack', store)
+    store.addShoes(['Nike', 'Adidas', 'Puma'])
+    expect(c1.buyShoes('Nike')).toBe('John bought Nike')
+    expect(c2.buyShoes('Adidas')).toBe('Jane bought Adidas')
+    expect(c3.buyShoes('Puma')).toBe('Jack bought Puma')
+  })
+  it("should not be buy a shoe if store have't shock", () => {
+    const store = new ShoesStore()
+    const c1 = new Customer('John', store)
+    expect(c1.buyShoes('Nike')).toBe('Shoe not in stock')
+  })
+  it('should be buy a shoe when have shock', () => {
+    const store = new ShoesStore()
+    const c1 = new Customer('John', store)
+    store.addShoes('Adidas')
+    expect(c1.buyShoes('Nike')).toBe('Shoe not in stock')
+    store.addShoes('Puma')
+    expect(c1.buyShoes('Nike')).toBe('Shoe not in stock')
+    store.addShoes('Nike')
+    expect(c1.buyShoes('Nike')).toBe('John bought Nike')
+    expect(c1.getShoes()).toEqual(['Nike'])
+  })
+})
